aa4d2c3733e1004000c5d684665fe93d0c522c37
Daniel Tabion <dctabion@gmail.com>
2016-01-13 22:29:26 -0600

    added mainCtrl and helloWorld for controller

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -2,6 +2,6 @@ angular.module('todoListApp')
 .directive('helloWorld', function(){
   return {
     template: "This is the hello world directive!"
-    restrict: "E"
+    // restrict: "E"
   };
 });
