187a0aef39346d8a88e904f9cfa37f8f0fcff3bc
Daniel Tabion <dctabion@gmail.com>
2016-01-18 15:43:15 -0600

    added ng-repeat to display all todos

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,14 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("Hello there! This is the helloWorld controller function, in the mainCtrl");
   };
+
+$scope.todos = [
+  {"name": "clean the house"},
+  {"name": "eat some sushi"},
+  {"name": "clean the bathtub"},
+  {"name": "throw computer out the window"},
+  {"name": "sleep a lot"}
+]
+
+
 });
