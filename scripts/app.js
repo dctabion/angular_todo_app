ee4fe3d7e993dfab9c416b5b998909498b4f431e
Daniel Tabion <dctabion@gmail.com>
2016-01-18 18:14:43 -0600

    added http req to get data from server

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,11 @@ angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope, dataService) {
   $scope.helloConsole = dataService.helloConsole;
 
+  dataService.getTodos(function(response){
+    console.log(response.data);
+    $scope.todos = response.data;
+  });
+
   $scope.helloWorld = function() {
     console.log("Hello there! This is the helloWorld controller function, in the mainCtrl");
   };
@@ -12,8 +17,13 @@ angular.module("todoListApp", [])
 
 })
 
-.service('dataService', function(){
+.service('dataService', function($http){
+
   this.helloConsole = function() {
     console.log('This is hello console service!');
-  }
+  };
+
+  this.getTodos = function(callback){
+    $http.get('mock/todos.json').then(callback);
+  };
 });
